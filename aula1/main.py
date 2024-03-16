5793da16c371d65bc78f12da667f567f4edad4b0
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-16 09:50:46 -0300

    att 16/03/2024

--- aula1/main.py
+++ aula1/main.py
@@ -3,3 +3,4 @@ idade = input('Qual é a sua idade: ')
 cidade = input('Onde você mora: ')
 
 print('\nO ' + nome + ' tem ' + str(idade) + ' anos de idade e mora em ' + cidade + '.') #convertendo variavel idade do tipo int para string no print
+#teste
