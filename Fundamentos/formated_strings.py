456ccc9ad62567b31eab0b71e9eaeb6d601d0f7b
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-17 12:45:20 -0300

    strings formatadas

---
+++ Fundamentos/formated_strings.py
@@ -0,0 +1,12 @@
+nome = 'Lucas'
+sobrenome = 'Antunes'
+profissao = 'Programador'
+
+texto ='O ' + nome + ' ' + sobrenome + ' é um excelente ' + '[' + profissao + ']' 
+
+texto2 = f'O {nome} {sobrenome} é um excelente [{profissao}]'
+
+print(texto)
+print(texto2)
+
+#O Lucas Antunes é um excelente [Programador]
