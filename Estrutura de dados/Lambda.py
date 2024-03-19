f80c2e0a358ee51825052fcd28079d9857d05391
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-19 20:13:50 -0300

    Lambda

--- Estrutura de dados/Lambda.py
+++ Estrutura de dados/Lambda.py
@@ -4,3 +4,10 @@
     #Muito utilizada dentro de outras funções
     #Código mais "Clean"
 
+# def somar(x):
+#     return x + 10
+# print(somar(2))
+
+
+somar10 = lambda x: x + 10
+print(somar10(2))
