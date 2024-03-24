30d4d60ebb5da63dc424428c7f9ebf4e0fff7bd2
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-24 18:42:10 -0300

    Desafio 1.2 - Resolução

--- Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-Pintura.py
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-Pintura.py
@@ -9,14 +9,22 @@ O programa deve mostrar na tela a mensagem 'Você necessita de x latas de tinta'
 
 # import math
 
-rendimento = float(input('Qual o rendimento da lata? '))
-altura = float(input('Qual a altura da parede? '))
-largura = float(input('Qual a largura da parede? '))
+rendimento = int(input('Qual o rendimento da lata? '))
+altura = int(input('Qual a altura da parede? '))
+largura = int(input('Qual a largura da parede? '))
 
-def latas(altura, largura, rendimento):
+# def latas(altura, largura, rendimento):
+#     area_parede = altura * largura
+#     return area_parede / rendimento
+
+# qtd_latas = latas(altura, largura, rendimento)
+
+#RESOLUÇÃO
+
+def latas():
     area_parede = altura * largura
-    return area_parede / rendimento
+    qtd_latas = area_parede / rendimento
+    print(f'Voce precisa de {qtd_latas} latas de tinta')
 
-qtd_latas = latas(altura, largura, rendimento)
-
-print(f'Voce precisa de {qtd_latas} latas de tinta')
+latas()
+# print(f'Voce precisa de {qtd_latas} latas de tinta')
