ec65339f7ab144a0c8b1843135f7f5a9dbd23a00
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-24 15:19:43 -0300

    Desafio 1 - Ponto do steak - Inicio

---
+++ Desafios - Atividades/Desafio1/Desafio1-Ponto-Steak.py
@@ -0,0 +1,15 @@
+#Desafio com If, Elif, Else
+
+'''
+Criar um programa que dependendo da temperatura (em celsius) do steak
+ele retorna o ponto de cozimento em português. O usuário deverá
+fornecer a temperatura.
+
+Temperatura - Cozimento
+120ºF ou 48ºC - Rare (Selada)
+130ºF ou 54ºC - Medium Rare (Ao ponto para o mal)
+140ºF ou 60ºC - Medium (Ao ponto)
+150ºF ou 65ºC - Medium Well (Ao ponto para o bem)
+160ºF ou 71ºC - Well Done (Bem passada)
+'''
+
