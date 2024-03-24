9a19088e5fc8eb0dc926cc9083f5911136cf9c12
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-24 18:16:07 -0300

    Inicio desafio 1.2

---
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-Pintura.py
@@ -0,0 +1,9 @@
+#Desafio com funções
+
+'''
+Criar um programa que calcula a quantidade de tinta necessária para
+pintar uma parede. O usuário deverá fornecer as seguintes
+informações: Rendimento, altura e largura.
+O programa deve mostrar na tela a mensagem 'Você necessita de x latas de tinta'
+'''
+
