3ac510b54ef52738129dab0717f03b2e18907817
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-31 18:29:25 -0300

    Desafio 1.3 - 1ª tentativa

---
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Filtro-Funcionarios.py
@@ -0,0 +1,9 @@
+#Desafio com 'Sets'
+
+'''
+Criar um programa que gere 3 listas de acordo com a necessidade logo abaixo:
+
+Lista 1 = Funcionários que tem carro e trabalham a noite
+Lista 2 = Funcionários que tem carro e trabalham durante o dia
+Lista 3 = Funcionários que não tem carro
+'''
