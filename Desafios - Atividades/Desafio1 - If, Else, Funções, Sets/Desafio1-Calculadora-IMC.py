b3cd28d7f7241a524945a238cff860f0be120eeb
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-31 19:02:07 -0300

    Desafio 1.4 - Calculadora de IMC - Inicio

---
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-IMC.py
@@ -0,0 +1,12 @@
+#Calculo de IMC - Índice de Massa Corporal
+
+'''
+Qual é a sua altura em cm:
+Qual é o seu peso em kg:
+'''
+
+# MENOR QUE 18,5 -- MAGREZA
+# ENTRE 18,5 E 24,9 -- NORMAL
+# ENTRE 25,0 E 29,9 -- SOBREPESO
+# ENTRE 30,0 E 39,9 -- OBESIDADE
+# MAIOR QUE 40,0 -- OBESIDADE GRAVE
