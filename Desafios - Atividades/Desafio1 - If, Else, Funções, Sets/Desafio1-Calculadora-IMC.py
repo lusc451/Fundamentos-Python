950844f6a85fb63f0e48d4dc9a66f158da180bf7
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-31 19:07:16 -0300

    Desafio 1.4 - 1ª tentativa

--- Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-IMC.py
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Calculadora-IMC.py
@@ -10,3 +10,9 @@ Qual é o seu peso em kg:
 # ENTRE 25,0 E 29,9 -- SOBREPESO
 # ENTRE 30,0 E 39,9 -- OBESIDADE
 # MAIOR QUE 40,0 -- OBESIDADE GRAVE
+
+altura = float(input('Qual é a sua altura em cm: '))
+peso = float(input('Qual é o seu peso em kg: '))
+imc = peso / (altura ** 2)
+
+print(imc)
