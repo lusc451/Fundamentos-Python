5ede43c11d2798a7f58b3ae12821d85b78685dd1
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-31 18:32:11 -0300

    Desafio 1.3 - 1ª tentativa

--- Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Filtro-Funcionarios.py
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Filtro-Funcionarios.py
@@ -7,3 +7,9 @@ Lista 1 = Funcionários que tem carro e trabalham a noite
 Lista 2 = Funcionários que tem carro e trabalham durante o dia
 Lista 3 = Funcionários que não tem carro
 '''
+
+funcionarios = ['Ana', 'Marcos', 'Alice', 'Pedro', 'Sophia', 'Bruno', 'Melissa']
+turno_dia = ['Ana', 'Marcos', 'Alice', 'Melissa']
+turno_noite = ['Pedro', 'Sophia', 'Bruno']
+tem_carro = ['Marcos', 'Alice', 'Bruno', 'Melissa']
+
