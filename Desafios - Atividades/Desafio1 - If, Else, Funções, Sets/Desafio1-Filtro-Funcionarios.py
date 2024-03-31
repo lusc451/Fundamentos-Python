6d1e6b78f99f0c3e849cae46636a55868f398045
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-03-31 18:57:41 -0300

    Desafio 1.3 - Resolução

--- Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Filtro-Funcionarios.py
+++ Desafios - Atividades/Desafio1 - If, Else, Funções, Sets/Desafio1-Filtro-Funcionarios.py
@@ -13,10 +13,24 @@ turno_dia = ['Ana', 'Marcos', 'Alice', 'Melissa']
 turno_noite = ['Pedro', 'Sophia', 'Bruno']
 tem_carro = ['Marcos', 'Alice', 'Bruno', 'Melissa']
 
-lista1 = set(funcionarios)
-lista2 = set(turno_dia)
-lista3 = set(tem_carro)
+# lista1 = set(funcionarios)
+# lista2 = set(turno_dia)
+# lista3 = set(tem_carro)
 
-print(lista1 - lista2 & lista3) #Funcionários que tem carro e trabalham a noite
-print(lista1 & lista2 & lista3) #Funcionários que tem carro e trabalham durante o dia
-print(lista1 - lista3) #Funcionários que não tem carro
+#print(lista1 - lista2 & lista3) #Funcionários que tem carro e trabalham a noite
+#print(lista1 & lista2 & lista3) #Funcionários que tem carro e trabalham durante o dia
+#print(lista1 - lista3) #Funcionários que não tem carro
+
+#RESOLUÇÃO
+
+#LISTA 1
+lista1 = set(tem_carro).intersection(turno_noite)
+print(lista1)
+
+#LISTA 2
+lista2 = set(tem_carro).intersection(turno_dia)
+print(lista2)
+
+#LISTA 3
+lista3 = set(funcionarios).difference(tem_carro)
+print(lista3)
